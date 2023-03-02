2bfae5701716db9a97e63f38d97df702a5634db0
redoprogrammer <redoprogrammer@gmail.com>
2023-03-02 22:19:04 +0700

    set route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Following from './pages/Following';
+
 function App() {
   return (
-    <h1>Hello world</h1>
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/following' element={<Following/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
