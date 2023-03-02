9df75b8283a63749f8024ab783725e7d20e30879
redoprogrammer <redoprogrammer@gmail.com>
2023-03-02 22:50:01 +0700

    set layout case in app.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { publicRoutes } from './routes';
 import DefaultLayout from './Layout/DefaultLayout';
-
+import { Fragment } from 'react';
 function App() {
   return (
     <Router>
       <div className='App'>
         <Routes>
           {publicRoutes.map((route, index) => {
-            const Layout = route.layout || DefaultLayout;// nếu trong route không set layout thì mặc định sẽ là defaultlayout
+            const Layout = route.layout === null? Fragment: DefaultLayout;// nếu trong route không set layout (null) thì dùng Fragement, ngược lại thì mặc định sẽ là defaultlayout
             const Page = route.component;
             return <Route key={index} path={route.path} element={
               <Layout>
