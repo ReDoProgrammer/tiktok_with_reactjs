1ced85a8db5a5347fff6161c23b3420f20a8c5cb
redoprogrammer <redoprogrammer@gmail.com>
2023-03-02 23:14:16 +0700

    reconfig layout in app.js

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
         <Routes>
           {publicRoutes.map((route, index) => {
             let Layout = DefaultLayout;
-            if(route.layout === null){
-              Layout = Fragment;
-            }else{
+            if(route.layout){
               Layout = route.layout;
+            }else if(route.layout === null){
+              Layout = Fragment;
             }
             const Page = route.component;
             return <Route key={index} path={route.path} element={
